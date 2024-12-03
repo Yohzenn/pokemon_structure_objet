6e368e2bcbc05c5ceb566204b417d043421f5154
Ilyan-Jude Bain-Trimbach <ilyanjudecour@gmail.com>
2024-12-03 15:28:28 +0100

    Correction de la logique de la capacite spe
    Degats negatif desormais impossible

--- class/PokemonEau.php
+++ class/PokemonEau.php
@@ -4,7 +4,7 @@ class PokemonEau extends Pokemon {
     public function capaciteSpeciale(Pokemon $adversaire): array
     {
         $bonus = $adversaire->getType() === "feu" ? 10 : 0;
-        $degats = ($this->getPuissanceAttaque() - $adversaire->getDefense()) +  $bonus;
+        $degats = max(0, ($this->getPuissanceAttaque() - $adversaire->getDefense()) + $bonus);
         $adversaire->recevoirDegats(max(0, $degats));
         return [$degats,$bonus];
     }
--- class/PokemonFeu.php
+++ class/PokemonFeu.php
@@ -4,7 +4,7 @@ class PokemonFeu extends Pokemon {
     public function capaciteSpeciale(Pokemon $adversaire): array
     {
         $bonus = $adversaire->getType() === "plante" ? 10 : 0;
-        $degats = $this->getPuissanceAttaque() + $bonus  - $adversaire->getDefense();
+        $degats = max(0, ($this->getPuissanceAttaque() - $adversaire->getDefense()) + $bonus);
         $adversaire->recevoirDegats(max(0, $degats));
         return [$degats,$bonus];
     }
