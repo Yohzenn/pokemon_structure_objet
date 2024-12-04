cd9423be98b8d5d7d045dcecb093f184f46036e3
Ilyan-Jude Bain-Trimbach <ilyanjudecour@gmail.com>
2024-12-04 22:55:51 +0100

    changement du front
    Apparition du texte progressif et impossible de spammer le btn entree

--- front_combat.php
+++ front_combat.php
@@ -50,116 +50,137 @@ if (isset($_GET['pokemon1_select']) && isset($_GET['pokemon2_select'])) {
 </body>
 <script>
     document.addEventListener("DOMContentLoaded", async () => {
-    // affichage des pokemons
-    const fetchPokemon = async (pokemonName) => {
-    const link = "https://pokeapi.co/api/v2/pokemon/" + pokemonName;
-    const repsonse = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-    );
+        // affichage des pokemons
+        const fetchPokemon = async (pokemonName) => {
+            const link = "https://pokeapi.co/api/v2/pokemon/" + pokemonName;
+            const repsonse = await fetch(
+                `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+            );
 
-    const data = await repsonse.json();
-    return data.sprites.front_default
-    };
+            const data = await repsonse.json();
+            return data.sprites.front_default;
+        };
 
-    const capitalizeFirstLetter = (text) => {
-        if (!text) return "";
+        const capitalizeFirstLetter = (text) => {
+            if (!text) return "";
             return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
-    }
+        };
 
-    // Affichage des dégats
-    const health_bar = (degats, pv, pv_max, pokemon_nb) =>{
-        if (pokemon_nb == 1){          
-            pv = Math.max(0, pv - degats);
-            setTimeout(() => {
-                document.getElementById('health-bar-fill1').style.width = `${(pv / pv_max) * 100}%`
-                document.getElementById('health-bar-text1').innerHTML = pv
-            }, 500);
-            
-        }
-        else{
-            pv = Math.max(0, pv - degats);
-            setTimeout(() => {
-                document.getElementById('health-bar-fill2').style.width = `${(pv / pv_max) * 100}%`
-                document.getElementById('health-bar-text2').innerHTML = pv
-            }, 500);
+        // Affichage des dégats
+        const health_bar = (degats, pv, pv_max, pokemon_nb) => {
+            if (pokemon_nb == 1) {
+                pv = Math.max(0, pv - degats);
+                setTimeout(() => {
+                    document.getElementById('health-bar-fill1').style.width = `${(pv / pv_max) * 100}%`;
+                    document.getElementById('health-bar-text1').innerHTML = pv;
+                }, 500);
+
+            } else {
+                pv = Math.max(0, pv - degats);
+                setTimeout(() => {
+                    document.getElementById('health-bar-fill2').style.width = `${(pv / pv_max) * 100}%`;
+                    document.getElementById('health-bar-text2').innerHTML = pv;
+                }, 500);
+            }
+            return pv;
+        };
+
+        // Apparition du texte 
+        const text_apparition = (text_container, text) => {
+    
+            if (!animated) {
+                animated = true; 
+
+                text_container.innerHTML = ""; 
+
         
-        }
-        return pv
-    }
-    
-
-    // Recuperation des infos
-    const tabCombatElement = document.getElementById('tab_combat');
-    const tabCombat = JSON.parse(tabCombatElement.getAttribute('data-combat'));
-
-    const gagnant = tabCombatElement.getAttribute('data-gagnant');
-    const pokemon1 = tabCombatElement.getAttribute('data-first');
-    const pokemon2 = tabCombatElement.getAttribute('data-sec');
-
-    const pokemon1PV_bar = document.getElementById("health-bar1");
-    const pokemon2PV_bar = document.getElementById("health-bar2");
-
-    let pokemon1PV = document.getElementById("health-bar-text1").textContent;
-    let pokemon2PV = document.getElementById("health-bar-text2").textContent;
-    
-    const pokemon1PV_max = pokemon1PV;
-    const pokemon2PV_max = pokemon2PV;
-
-    const info = document.querySelector('.deroulement');
-
-    const img1 = document.getElementById("img1");
-    const img2 = document.getElementById("img2")
-
-    img1.src = await fetchPokemon(pokemon1);
-    img2.src = await fetchPokemon(pokemon2);
-
-    info.innerHTML = `Le combat de ${pokemon1} contre ${pokemon2} va commencer`;
-
-    // Combat
-    let i = 0;
-    let toggle = true;
-    document.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
-            if (i < tabCombat.length){
-                
-                if (toggle){
-                    toggle = !(toggle)
-                    pokemon2PV = health_bar(tabCombat[i][1],pokemon2PV,pokemon2PV_max,2)
-                    img1.classList.add("attaque1")
-                    img1.classList.remove("degat")
-                    img2.classList.add("degat")
-                    img2.classList.remove("attaque2")
-
-                    pokemon1PV_bar.classList.remove("degat")
-                    pokemon2PV_bar.classList.add("degat")
+                for (let i = 0; i < text.length; i++) {
+                    setTimeout(() => {
+                        text_container.textContent += text[i];
+                    }, 15 * i);
                 }
-                else{
-                    toggle = !(toggle)
 
-                    pokemon1PV = health_bar(tabCombat[i][1],pokemon1PV,pokemon1PV_max,1)
-                    img1.classList.remove("attaque1")
-                    img1.classList.add("degat")
-                    img2.classList.remove("degat")
-                    img2.classList.add("attaque2")
+        
+                setTimeout(() => {
+                    animated = false; 
+                }, 15 * text.length); 
+            }
+        };
 
-                    pokemon1PV_bar.classList.add("degat")
-                    pokemon2PV_bar.classList.remove("degat")
-                }
-                info.innerHTML = capitalizeFirstLetter(tabCombat[i][0])
-                i ++
-            }     
-            else{
-                info.innerHTML = capitalizeFirstLetter(`${gagnant} a gagner !`)
-                document.addEventListener("keydown", function(event) {
-                    if (event.key === "Enter") {
-                       window.location.href = "./index.php"
+        // Recuperation des infos
+        const tabCombatElement = document.getElementById('tab_combat');
+        const tabCombat = JSON.parse(tabCombatElement.getAttribute('data-combat'));
+
+        const gagnant = tabCombatElement.getAttribute('data-gagnant');
+        const pokemon1 = tabCombatElement.getAttribute('data-first');
+        const pokemon2 = tabCombatElement.getAttribute('data-sec');
+
+        const pokemon1PV_bar = document.getElementById("health-bar1");
+        const pokemon2PV_bar = document.getElementById("health-bar2");
+
+        let pokemon1PV = document.getElementById("health-bar-text1").textContent;
+        let pokemon2PV = document.getElementById("health-bar-text2").textContent;
+
+        const pokemon1PV_max = pokemon1PV;
+        const pokemon2PV_max = pokemon2PV;
+
+        const info = document.querySelector('.deroulement');
+
+        const img1 = document.getElementById("img1");
+        const img2 = document.getElementById("img2");
+
+        img1.src = await fetchPokemon(pokemon1);
+        img2.src = await fetchPokemon(pokemon2);
+
+        // Combat
+        let animated = false;
+        let i = 0;
+        let toggle = true;
+        let first = true;
+        info.innerHTML="";
+
+        setTimeout(() => {
+            text_apparition(info, `Le combat de ${pokemon1} contre ${pokemon2} va commencer !`);
+        }, 15);
+        document.addEventListener("keydown", function(event) {
+            if (event.key === "Enter" && !animated) {
+                if (i < tabCombat.length) {
+                    if (toggle) {
+                        toggle = !(toggle);
+                        pokemon2PV = health_bar(tabCombat[i][1], pokemon2PV, pokemon2PV_max, 2);
+                        img1.classList.add("attaque1");
+                        img1.classList.remove("degat");
+                        img2.classList.add("degat");
+                        img2.classList.remove("attaque2");
+
+                        pokemon1PV_bar.classList.remove("degat");
+                        pokemon2PV_bar.classList.add("degat");
+                    } else {
+                        toggle = !(toggle);
+
+                        pokemon1PV = health_bar(tabCombat[i][1], pokemon1PV, pokemon1PV_max, 1);
+                        img1.classList.remove("attaque1");
+                        img1.classList.add("degat");
+                        img2.classList.remove("degat");
+                        img2.classList.add("attaque2");
+
+                        pokemon1PV_bar.classList.add("degat");
+                        pokemon2PV_bar.classList.remove("degat");
                     }
-                })
-            } 
-        }
+                    text_apparition(info, capitalizeFirstLetter(tabCombat[i][0])); 
+                    i++;
+                } else if (first) {
+                    first = !first;
+                    text_apparition(info, capitalizeFirstLetter(`${gagnant} a gagner !`)); 
+                    document.addEventListener("keydown", function(event) {
+                        if (event.key === "Enter") {
+                            window.location.href = "./index.php";
+                        }
+                    });
+                } 
+            }
         });
-    })
-
-
+    });
 </script>
+
 </html>
