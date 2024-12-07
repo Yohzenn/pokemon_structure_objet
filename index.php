0f83097fec29a12664feacde6d2ee3fb6b8a05e2
Ilyan-Jude Bain-Trimbach <ilyanjudecour@gmail.com>
2024-12-07 13:18:33 +0100

    ajout du front de presentation du combat

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once "./pokemon_ini.php";
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     <script src="https://kit.fontawesome.com/ec35368b02.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./src/css/style.css">
-    <!--<link rel="stylesheet" href="./src/css/index.css"> -->
+    <link rel="stylesheet" href="./src/css/affichage.css">
 </head>
 <body>
     <main>
@@ -21,12 +21,70 @@ require_once "./pokemon_ini.php";
         if (isset($_GET['pokemon1_select']) && isset($_GET['pokemon2_select']) && $_GET['pokemon1_select'] != $_GET['pokemon2_select']) {
             $pokemon1 = $pokemonTab[htmlspecialchars($_GET['pokemon1_select'])];
             $pokemon2 = $pokemonTab[htmlspecialchars($_GET['pokemon2_select'])];
-
-            echo "<h2>Combat entre : </h2>";
-            echo "<p>Pokemon 1 : " . $pokemon1->getNom() . "</p>";
-            echo "<p>Pokemon 2 : " . $pokemon2->getNom() . "</p>";
             $link = './front_combat.php?pokemon1_select=' . $_GET['pokemon1_select'] . '&pokemon2_select=' . $_GET['pokemon2_select'];
-            echo "<a href='".$link."'>Lancer le combat</a>";
+            $pokemons = [$pokemon1,$pokemon2];
+            echo '<h2 class="vs">VS</h2>';
+
+        ?>  
+            <div class="fond2">
+            <div class="bg">
+            <div class="container" id="container">
+                <?php
+                $i = 0;
+                foreach ($pokemons as $pokemon) {            
+                ?>
+                <div class="card swiper-slide <?php echo $pokemon->getType()?>">
+                    <input type="hidden" id="id_pokemon" value=<?php echo $i ?>>
+                    <div id="top">
+                        <h3 id="nom"><?= ucfirst($pokemon->getNom()); ?> </h3>
+                        <h3 id="pv"><?= $pokemon->getPointsDeVie(); ?> PV</h3>
+                    </div>
+                    <?php 
+                    $url = "https://pokeapi.co/api/v2/pokemon/" . $pokemon->getNom();
+                    $response = file_get_contents($url);
+                    $data = json_decode($response, true);
+                    ?>
+                    <img src="<?= $data['sprites']['front_default'];?>" id="pokemon-image" alt="">
+                    <div id="type-holder">
+                        <h3 id="type-name">Type:</h3>
+                        <h3 id="type"><?= $pokemon->getType(); ?></h3>
+                    </div>
+                    <div id="faiblesse-holder">
+                        <h3 id="faiblesse-name">Faiblesse:</h3>
+                        <h3 id="faiblesse">
+                            <?php if( $pokemon->getType() === 'feu'){
+                                    echo 'Eau';
+                                    } 
+                                else if( $pokemon->getType() === 'eau'){
+                                    echo 'Plante';
+                                    } 
+                                else{
+                                    echo 'Feu';
+                                    } 
+                            ?>
+                        </h3>
+                    </div>
+                    <div id="attaque-holder">
+                        <h3 id="attaque-name">Attaque:</h3>
+                        <h3 id="attaque"><?= $pokemon->getPuissanceAttaque(); ?></h3>
+                    </div>
+                    <div id="defense-holder">
+                        <h3 id="defense-name">Defense:</h3>
+                        <h3 id="defense"><?= $pokemon->getDefense(); ?></h3>
+                    </div>
+                </div>
+                <?php 
+                    $i ++;
+                }
+                ?>
+            </div>  
+
+        <?php
+            echo "<a id='combattre' href='".$link."'>Lancer le combat</a>";
+        ?>
+        </div>
+        </div> 
+        <?php
         } else {
             ?>
             <div class="swiper">
